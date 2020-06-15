3eb1e0beb52823342d1afd6472b2f892f89ae988
Herdanis <herdanis.pradana@gmail.com>
2020-06-15 20:55:22 +0700

    input setting waktu

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ import schedule
 import time
 
 
-# print(time.ctime())
-
 listWeb = ['intentA', 'intentB', 'intentC', 'intentD',
            'intentE', 'opdA', 'opdB', 'opdC', 'opdD', 'opdE']
 listWebTest = ['intentA', 'intentB', 'opdA', 'opdB']
 
+hours = input("Setting Cek Per Jam ? (Integer) ")
+print("sistem akan mengecek " + hours + " sekali")
+
 
 def cek():
-    for listW in listWeb:
+    for listW in listWebTest:
         webchecker.webChecker(listW)
 
 
 cek()
 
-schedule.every().hours.at(':01').do(cek)
+schedule.every(int(hours)).hours.at(':01').do(cek)
 while True:
     schedule.run_pending()
     time.sleep(1)
